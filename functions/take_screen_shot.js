4a985941700d8b9e5b0c136a565b90f2585cae89
John Williams <jrw+3@mit.edu>
2024-01-04 22:23:33 +0000

    Puppeteer issue

--- functions/take_screen_shot.js
+++ functions/take_screen_shot.js
@@ -1,10 +1,12 @@
 const PuppeteerWebBaseLoader = require ("langchain/document_loaders/web/puppeteer");
 const fs = require( "fs");
 const path = require("path");
+const puppeteer = require('puppeteer');
 
-const execute = async ({ url }) => {
+const execute = async (url ) => {
   try {
-    const websiteName = new URL(url).hostname;
+    //const websiteName = new URL(url).hostname;
+    const websiteName = url;
     const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
 
     const outputDirectory = path.join(".", "output");
@@ -17,8 +19,9 @@ const execute = async ({ url }) => {
       outputDirectory,
       screenshotFileName
     );
+    let browser = await puppeteer.launch();
 
-    const loader = new PuppeteerWebBaseLoader(url, {
+    const PuppeteerOptions = {
       launchOptions: {
         headless: "new",
       },
@@ -31,7 +34,8 @@ const execute = async ({ url }) => {
         });
         return `Screenshot saved to ${screenshotFilePathWithDate}`;
       },
-    });
+    }
+    const loader =  new PuppeteerWebBaseLoader(url, PuppeteerOptions);
 
     await loader.load();
 
